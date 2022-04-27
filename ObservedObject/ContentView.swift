af54b1d855362c1e678956abf88494e2c0eb5237
lee <2014272050@yonsei.ac.kr>
2022-04-27 17:06:34 +0900

    contentView에서 root를 올리면 state를 바꾸므로 계속 뷰 바뀌고, 뷰모델, 카운트뷰 이니셜라이즈됨

--- ObservedObject/ContentView.swift
+++ ObservedObject/ContentView.swift
@@ -7,10 +7,50 @@
 
 import SwiftUI
 
+class CountViewModel: ObservableObject {
+    
+  @Published var count: Int = 0
+    
+  init() {
+    print("ViewModel init!")
+  }
+    
+  func addCount() {
+    count += 1
+  }
+    
+}
+
 struct ContentView: View {
+    
+    @State private var rootCount: Int = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            CountView(rootCount: rootCount)
+            Button(action: {rootCount += 1}, label: {
+                Text("Root Add")
+            })
+        }
+    }
+}
+
+struct CountView: View {
+    @ObservedObject var viewModel = CountViewModel()
+    init(rootCount:Int){
+        self.rootCount = rootCount
+        print("init~")
+    }
+    
+    let rootCount: Int
+    
+    var body: some View {
+        VStack {
+            Text("Root: \(rootCount)")
+            Text("Count: \(viewModel.count)")
+            Button(action: {viewModel.addCount()}, label: {
+                Text("Countedr Add")
+            })
+        }
     }
 }
 
