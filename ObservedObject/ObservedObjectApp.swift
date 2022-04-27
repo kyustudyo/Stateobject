c3e76afb6ba2aa8debec9ffc6007664441f03987
lee <2014272050@yonsei.ac.kr>
2022-04-27 16:26:03 +0900

    Initial Commit

---
+++ ObservedObject/ObservedObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservedObjectApp.swift
+//  ObservedObject
+//
+//  Created by Hankyu Lee on 2022/04/27.
+//
+
+import SwiftUI
+
+@main
+struct ObservedObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
